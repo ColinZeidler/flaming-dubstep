6fe2690f051d11f73891cd6f6bf1527e213d8b59
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-06-21 14:16:06 -0400

    cleaning up look of the ui

--- callStats.py
+++ callStats.py
@@ -67,43 +67,65 @@ streams = []
 
 r = requests.get("http://{}:{}/ui/services/system/stream-stats".format(ip, port), auth = ("user", "video123"))
 b = requests.get("http://{}:{}/ui/services/system/build-info".format(ip, port), auth = ("user", "video123"))
+try:
+	for item in r.json()['streams-incoming-video']:
+		streams.append(item)
+except:
+	streams.append("")
 
-for item in r.json()['streams-incoming-video']:
-	streams.append(item)
-
+Dcount = 0
 print "<h2>System Stats</h2>"
 print "<div id=buildInfo>Build: {}</br>Built: {}</div>".format(b.json()['version'], b.json()['build_date'])
 print "<h3>Incoming Video</h3>"
 print "<div id=statsContent>"
-for stream in streams:
-	print "<div id=SysStat><b>"
-	print names[stream['location-id']]
-	print "</b>"
-	for vid in stream['streams-incoming-video-status']:
-		print "<div id=stat>"
-		print "<b>" + vid['title'] +"</b>"
-		print "<p>"
-		try:
-			print vid['source-fps'], "FPS"
-		except:
-			print vid['fps'], "FPS"
-		try:
-			print "</br>{}p".format(vid['source-height'])
-		except:
-			pass
-		print "</p>"
-		print "</div>" #end of stat
-	print "</div>" #end of SysStat
+try:
+	for stream in streams:
+		print "<div id=SysStat><b>"
+		Dcount += 1
+		print names[stream['location-id']]
+		print "</b>"
+		for vid in stream['streams-incoming-video-status']:
+			print "<div id=stat>"
+			Dcount += 1
+			print "<b>" + vid['title'] +"</b>"
+			print "<p>"
+			try:
+				print vid['source-fps'], "FPS"
+			except:
+				print vid['fps'], "FPS"
+			try:
+				print "</br>{}p".format(vid['source-height'])
+			except:
+				pass
+			print "</p>"
+			print "</div>" #end of stat
+			Dcount -= 1
+		print "</div>" #end of SysStat
+		Dcount -= 1
+except:
+	for i in range(0, Dcount):
+		print "</div>"
+	print "<h4>No Incoming Video</h4>"
 print "</div>" #end of statsContent
 print "<h3>Outgoing Video</h3>"
 print "<div id=outVidContent>"
 #for loop over all out going streams
-for item in r.json()['streams-outgoing-video']:
-	print "<div id=SysStat><b>{}</b>".format(names[item['location-id']])
-	for stream in item['streams-outgoing-video-status']:
-		print "<div id=stat>"
-		print "<b>{}</b>".format('Video' if stream['resource-id'] == 'video' else 'Collab')
-		print "<p>{} FPS</br>{}p</p>".format(stream['fps'], stream['height'])
-		print "</div>" #end of stat
-	print "</div>" #end of SysStat
+Dcount = 0
+try:
+	for item in r.json()['streams-outgoing-video']:
+		print "<div id=SysStat><b>{}</b>".format(names[item['location-id']])
+		Dcount += 1
+		for stream in item['streams-outgoing-video-status']:
+			print "<div id=stat>"
+			Dcount += 1
+			print "<b>{}</b>".format('Video' if stream['resource-id'] == 'video' else 'Collab')
+			print "<p>{} FPS</br>{}p</p>".format(stream['fps'], stream['height'])
+			print "</div>" #end of stat
+			Dcount -= 1
+		print "</div>" #end of SysStat
+		Dcount -= 1
+except:
+	for i in range(0, Dcount):
+		print "</div>"
+	print "<h4>No Out Going Video</h4>"
 print "</div>" #end of outVidContent
