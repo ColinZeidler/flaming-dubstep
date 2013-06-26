07332f8cfc20620653251f3412eef89edb989954
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-06-26 11:35:12 -0400

    changed image and size, added normal vs aux cam count

--- Info.py
+++ Info.py
@@ -7,6 +7,6 @@ ip, port = systemSort.getAddr(name)
 r = requests.get("http://{}:{}/ui/services/system/info".format(ip, port), auth=("user", "video123"))
 
 print "<p><b>Screens:</b>", r.json()['num-screens'], "</br>"
-print "<b>Cameras:</b>", r.json()['num-cams'], "</br>"
+print "<b>Cameras:</b> {},".format(r.json()['num-cams']),"<b>Normal:</b> {},".format(r.json()['num-norm-cams']), "<b>Aux:</b>", r.json()['num-aux-cams'], "</br>"
 print "<b>HDShare:</b>", r.json()['hdshare-url'], "</br>"
 print "<b>URL:</b>", r.json()['sip-url-public'] or r.json()['sip-url'], "</br></p>"
