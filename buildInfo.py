5a68b266d667ea20291d75bf7f8acfc8679a91e7
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-06-28 14:47:32 -0400

    shorted built tag, time of build was not neccesary

--- buildInfo.py
+++ buildInfo.py
@@ -6,4 +6,4 @@ ip, port = systemSort.getAddr(name)
 r = requests.get("http://{}:{}/ui/services/system/build-info".format(ip, port), auth=("user", "video123"))
 
 print "Build:", r.json()['version'],"</br>"
-print "Built:", r.json()['build_date']
+print "Built:", r.json()['build_date'].split("_")[0]
