df8af9525f7aa939b1c04d051ad7e6c116d1b896
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-07-05 10:37:14 -0400

    stat dropdown can only be toggled with the open/close bit now

--- addStat.php
+++ addStat.php
@@ -18,9 +18,9 @@ echo "<h2>System Stats</h2>
 	</select>
 	<div class=statButton onclick='resize(this)'><div class=statButtonTop><img src=images/expand.png  width=36 /></div><img src=images/expand-bottom.png  width=36 /></div>
 	<div id=\"buildInfo\">Build data</div>
-	<div class=sysDropDown onclick='sysDropDown(this)'>
+	<div class=sysDropDown >
 		<div class=systemInfo>Hi hi ipsum lorem toldar, ipsum loding index javascript</br> hi2</div>
-		<div class=sysDropTab>OPEN<img src=images/info-arrow.png  /> </div>
+		<div class=sysDropTab onclick='sysDropDown(this.parentNode)'>OPEN<img src=images/info-arrow.png  /> </div>
 	</div>
 	<div class=closeButtonBottom></div>
 	<div class=closeButton onclick='closeStat(this)'></div>
