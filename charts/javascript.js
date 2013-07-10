9d2d9b40e8fb8683308c5e02e00f77a319184143
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-07-10 10:08:22 -0400

    adding multiple charts, working

--- charts/javascript.js
+++ charts/javascript.js
@@ -3,25 +3,17 @@ var xArray = ["120","118","116","114","112","110","108","106","104","102","100",
                "72","70","68","66","64","62","60","58","56","54","52","50","48",
                "46","44","42","40","38","36","34","32","30","28","27","26","24",
                "22","20","18","16","14","12","10","8","6","4","2","0"];
-var bitArray = [[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],
-                [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0]];
+
 var System = "Avocado";
 var graphList = [];
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(function() {newSection(System);});
 
 function newSection(sys) {
- var parentDiv = document.getElementById('allSystems');
+ var parentDiv = document.getElementById('floatBox');
  var newContent = document.createElement('div');
  newContent.className = 'stats';
- newContent.innerHTML = "   <h1>Testing charts</h1>   <p>This is the test of the Google charts, with auto updates</p>   <div id=chart style=\"width:900px; height:600px;\"></div>"
+ newContent.innerHTML = "   <select id=\"systemSelector\" onchange='loadSysName(this)'>   <option value=\"Avocado\">Avocado</option>   <option value=\"Banana\">Banana</option>   <option value=\"Coconut\">Coconut</option>   <option value=\"Duku\">Duku</option>   <option value=\"Emblic\">Emblic</option>   <option value=\"Fig\">Fig</option>   <option value=\"Galia\">Galia</option>   <option value=\"Honeydew\">Honeydew</option>   <option value=\"Imbe\">Imbe</option>   <option value=\"Jalapeno\">Jalapeno</option>   <option value=\"Kiwi\">Kiwi</option>   <option value=\"Loganberry\">Loganberry</option>   <option value=\"Newton\">Newton</option>   <option value=\"Papaya\">Papaya</option>  </select>  <div id=chart style=\"width:900px; height:500px;\"></div>"
  newContent.id = sys;
  var child = parentDiv.appendChild(newContent);
  graphList.push(new Graph(child));
@@ -43,13 +35,13 @@ function Graph(sysDiv) {
                 [0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0]];
  this.data;
  var children = this.div.childNodes;
- for (var i = 0; i < children; i++) {
+ for (var i = 0; i < children.length; i++) {
   if (children[i].id == "chart") {
    var childDiv = children[i];
   }
  }
  this.chart = new google.visualization.LineChart(childDiv);
- this.options = {title: 'System Bit Rate', hAxis: {title: 'Seconds Ago'}, vAxis: {title: 'kbps', minValue: 0}, backgroundColor: '#F1F1EB'};
+ this.options = {title: 'Avocado Bit Rate', hAxis: {title: 'Seconds Ago'}, vAxis: {title: 'kbps', minValue: 0}, backgroundColor: '#F1F1EB'};
  console.info(this.name + " created");
 }
 
@@ -66,10 +58,8 @@ Graph.prototype.updateData = function() {
    var temp = xmlhttp.responseText.split(", ");
    temp[0] = parseInt(temp[0]);
    temp[1] = parseInt(temp[1]);
-   console.info(temp);
-   console.info(graphItem.bitArray);
-   console.info(graphItem.name);
-   console.info(graphItem.div);
+
+
    //push new data onto the bit array
    graphItem.bitArray.push(temp);
    //dataArray.shift() //removes old data
@@ -97,5 +87,18 @@ Graph.prototype.updateData = function() {
 
 Graph.prototype.changeName = function(name) {
  this.name = name;
+ this.div.id = name;
+ this.options = {title: name +' Bit Rate', hAxis: {title: 'Seconds Ago'}, vAxis: {title: 'kbps', minValue: 0}, backgroundColor: '#F1F1EB'};
 }
 
+function loadSysName(obj) {
+ var parentDiv = obj.parentNode;
+ 
+ //get Graph based on div
+ for (var i = 0; i < graphList.length; i ++) {
+  if (graphList[i].div == parentDiv) {
+   graphList[i].changeName(obj.options[obj.selectedIndex].value);
+   break;
+  }
+ }
+}
