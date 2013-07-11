5d3acba1e190537eb738c3f201d11c80bcdaef29
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-07-11 09:14:05 -0400

    added try catch block

--- charts/bitRate.py
+++ charts/bitRate.py
@@ -14,22 +14,27 @@ r = requests.get("http://{}:{}/ui/services/system/stream-stats".format(ip, port)
 
 totalIn = 0
 totalOut = 0
-
 try:
     for stream in r.json()['streams-incoming-video']:
         if "Self" != names[stream['location-id']]:
             for vid in stream['streams-incoming-video-status']:
                 if "mini" not in vid['title']: 
-                    totalIn += int(vid['source-kbps'])
-except:
-    totalIn = 0
+                    try:
+                        totalIn += int(vid['source-kbps'])
+                    except KeyError:
+                        totalIn += 0
+except KeyError:
+    pass
 
 try:
     for item in r.json()['streams-outgoing-video']:
         if "Self" != names[item['location-id']]:
             for stream in item['streams-outgoing-video-status']:
-                totalOut += int(stream['kbps'])
-except:
-    totalOut = 0;
+                try:
+                    totalOut += int(stream['kbps'])
+                except KeyError:
+                    totalOut += 0
+except KeyError:
+    pass
 
 print "{}, {}".format(totalOut, totalIn)
