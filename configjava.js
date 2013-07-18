a6cc2af61ddabb514c1a1cfc2da592ff6248ccca
Colin Zeidler <colin.zeidler@magorcorp.com>
2013-07-18 09:54:13 -0400

    read the info in inputs when button is clicked

--- configjava.js
+++ configjava.js
@@ -21,7 +21,7 @@ function readData() {
    var passData = document.createElement('div');
    passData.className = "passport";
 
-   passData.innerHTML = "<table> <tr><td>Name:</td><td><input type = text name = " + passport+"Name value = " + passport + "></td></tr> <tr><td>IP:</td><td><input type  = text name = "+ passport +"IP value = \"" + pp[passport].ip + "\"></td></tr></table>";
+   passData.innerHTML = "<table><tr><td>Name:</td><td><input type = text name = ppName value = " + passport + "></td></tr><tr><td>IP:</td><td><input type  = text name = ppIP value = \"" + pp[passport].ip + "\"></td></tr></table>";
    PassDiv.appendChild(passData);
   }
  }
@@ -33,9 +33,42 @@ function readData() {
    var sysData = document.createElement('div');
    sysData.className = "system";
 
-   sysData.innerHTML = "<table><tr><td>Name:</td><td><input type = text name = system1 value = " +sys[system].name+ "></td></tr><tr><td>IP:</td><td><input type = text name = sysIp1 value = " +sys[system].ip+ "></td></tr><tr><td>Passport:</td><td><input type = text name = sysPP1 value = " + sys[system].passport+ "></td></tr><tr><td>Port:</td><td><input type = number name = sysPort1 value = " + sys[system].port+ "></td></tr></table>";
-   console.info(system);
+   sysData.innerHTML = "<table><tr><td>Name:</td><td><input type = text name = system value = " +sys[system].name+ "></td></tr><tr><td>IP:</td><td><input type = text name = sysIp value = " +sys[system].ip+ "></td></tr><tr><td>Passport:</td><td><input type = text name = sysPP value = " + sys[system].passport+ "></td></tr><tr><td>Port:</td><td><input type = number name = sysPort value = " + sys[system].port+ "></td></tr></table>";
+//   console.info(system);
    SysDiv.appendChild(sysData);
   }
  }
+}
+
+function submit() {
+ var PassChildren = document.getElementById('Passports').childNodes;
+ var SysChildren = document.getElementById('Systems').childNodes;
+ 
+ for (var i = 0; i < SysChildren.length; i++) { //iterates over each system
+//  console.info(SysChildren[i]);
+  if (SysChildren[i].className == "system") { 
+   //             system class   table         table body    table rows    
+   //console.info(SysChildren[i].childNodes[0].childNodes[0].childNodes);
+   var rows = SysChildren[i].childNodes[0].childNodes[0].childNodes;
+   for (var a = 0; a < rows.length; a++) { //iterates over the inputs for each system
+    //console.info(rows[a].childNodes[1].childNodes[0].name);
+    var input = rows[a].childNodes[1].childNodes[0]; //use .name and .value
+   }
+  }
+ }
+
+ for (var i = 0; i < PassChildren.length; i++) { //iterates over each system
+//  console.info(SysChildren[i]);
+  if (PassChildren[i].className == "passport") { 
+   //             system class   table         table body    table rows    
+   //console.info(PassChildren[i].childNodes[0].childNodes[0].childNodes);
+   var rows = PassChildren[i].childNodes[0].childNodes[0].childNodes;
+   for (var a = 0; a < rows.length; a++) { //iterates over the inputs for each system
+    //console.info(rows[a].childNodes[1].childNodes[0].name);
+    //console.info(rows[a].childNodes[1].childNodes[0].value);
+    var input = rows[a].childNodes[1].childNodes[0]; //use .name and .value
+   }
+  }
+ }
+
 }
